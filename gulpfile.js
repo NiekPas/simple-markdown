c4b960061cc75d438116a9c45f92c95c900fc149
Niek van de pas <Niek_pas@live.nl>
2016-11-21 16:28:18 +0100

    Update gulpfile to integrate browser-sync into workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp =          require('gulp');
 var sass =          require('gulp-sass');
 var sourcemaps =    require('gulp-sourcemaps');
-var shell =         require('gulp-shell')
+var shell =         require('gulp-shell');
+var browserSync =   require('browser-sync').create();
+var runSequence =   require('run-sequence');
+
+// Static server
+gulp.task("browser-sync", function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
+    gulp.watch(["*.html", "css/*.scss", "js/*.ts"]).on("change", browserSync.reload);
+});
 
 gulp.task('sass', function() {
     return gulp.src('./css/main.scss')
@@ -13,7 +25,13 @@ gulp.task('sass', function() {
 
 gulp.task('typescript', shell.task('tsc'));
 
-gulp.task('default', function() {
-    gulp.watch('css/*.scss', ['sass']);
-    gulp.watch('js/**/*.ts', ['typescript']);
+gulp.task("default", function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(["./js/**/*.ts", "./css/*.scss", "./index.html"], function () {
+        runSequence(["sass", "typescript"], browserSync.reload);
+    });
 });
